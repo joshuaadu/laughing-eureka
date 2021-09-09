d6d8a0b841bf9e710e89755046a4db4186e8e64f
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2021-09-09 19:56:08 +0000

    feat: Update UI with destination data

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,21 @@
 import { serverRequest } from "./makeServerRequest"
+import { dateCountdown } from "./date"
+import { updateUI } from "./updateUI"
 
 const handleSubmit = (event) => {
     event.preventDefault()
     serverRequest(getFormData())
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
 }
 
 // Get user input from form
 const getFormData = () => {
+    const destination = document.getElementById('destination').value
+    const date = document.getElementById('date').value
     return {
-        destination: document.getElementById('destination').value,
-        date: document.getElementById('date').value
+        destination: destination,
+        date: date,
+        countDown: dateCountdown(date)
     }
 }
 
---
+++ src/client/js/updateUI.js
@@ -0,0 +1,29 @@
+const updateUI = (data) => {
+    const section = document.querySelector('.trip-display-section')
+
+    section.innerHTML = `
+        <div class="trip-info-card">
+        <div class="destination-geo-info">
+            <img class="destination-image" src="${data.image}" alt="Photo of ${data.name}">
+            <ul class="main-info">
+                <p class="destination-name">${data.name}, ${data.countryName}</p>
+                <p class="trip-date">Trip is ${data.countDown}</p>
+            </ul>
+            <ul class="temp-info">
+                <li class="temp">${data.temp}</li>
+                <li class="">
+                    <img class="destination-image" src="https://www.weatherbit.io/static/img/icons/${data.icon}.png" alt="${data.weather} icon">
+                </li>
+            </ul>
+        </div>
+        <div class="weather-highlight-div">
+            
+        </div>
+        </div>`
+
+    console.log(data)
+
+    
+}
+
+export {updateUI}
