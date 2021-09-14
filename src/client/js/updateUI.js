5365073b5da388c1702cbfb03cfe1a69b5bb80bd
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2021-09-14 20:22:04 +0000

    feat Add colors and weather highlights

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -1,23 +1,43 @@
 const updateUI = (data) => {
 	const section = document.querySelector(".trip-display-section");
-
+	const date = new Date(data.date);
+	const dateString = new Intl.DateTimeFormat("en-GB", {
+		dateStyle: "full",
+	}).format(date);
 	section.innerHTML = `
         <div class="trip-info-card">
         <div class="destination-geo-info">
             <img class="destination-image" src="${data.image}" alt="Photo of ${data.name}">
             <ul class="main-info">
-                <p class="destination-name">${data.name}, ${data.countryName}</p>
-                <p class="trip-date">Trip is ${data.countDown}</p>
+                <li class="destination-name">${data.name}, ${data.countryName}</li>
+                <li class="trip-countdown">Trip is ${data.countDown}</li>
+                <li class="trip-date">${dateString}</li>
             </ul>
             <ul class="temp-info">
-                <li class="temp">${data.temp}</li>
                 <li class="">
                     <img class="weather-icon" src="https://www.weatherbit.io/static/img/icons/${data.icon}.png" alt="${data.weather} icon">
                 </li>
+                <li class="temp">${data.temp}</li>
             </ul>
         </div>
         <div class="weather-highlight-div">
-            
+            <ul class="hightlight-block">
+                <li class="title">Wind staus</li>
+                <li class="value">${data.wind_speed}mph</li>
+                <li class="value">${data.wind_dir_code}</li>
+            </ul>
+            <ul class="hightlight-block">
+                <li class="title">Air Pressure</li>
+                <li class="value">${data.pressure}</li>
+            </ul>
+            <ul class="hightlight-block">
+                <li class="title">Humidity</li>
+                <li class="value">${data.humidity}%</li>
+            </ul>
+            <ul class="hightlight-block">
+                <li class="title">Population</li>
+                <li class="value">${data.population}</li>
+            </ul>
         </div>
         </div>`;
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,113 +1,116 @@
-var path = require('path')
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const dotenv = require('dotenv');
+var path = require("path");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 dotenv.config();
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-  
-const app = express()
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(express.static('dist'))
+const fetch = (...args) =>
+	import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("dist"));
 
-console.log(__dirname)
+console.log(__dirname);
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('dist/index.html'))
-})
+app.get("/", function (req, res) {
+	// res.sendFile('dist/index.html')
+	res.sendFile(path.resolve("dist/index.html"));
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8082, function () {
-    console.log('Example app listening on port 8082!')
-})
+	console.log("Example app listening on port 8082!");
+});
 
+app.post("/travel-info", (req, res) => {
+	let destinationData = {};
+	const destination = req.body.destination;
+	destinationData.countDown = req.body.countDown;
+	destinationData.date = req.body.date;
 
-app.post('/travel-info', (req, res) => {
-    let destinationData = {}
-    const destination = req.body.destination
-    destinationData.countDown = req.body.countDown
+	// Get geographic data
+	getGeoData(destination)
+		.then((data) => {
+			// Save geo data to destination data
+			Object.assign(destinationData, data.geonames[0]);
+			// console.log(data.geonames[0])
+			console.log(destinationData);
+			return [data.geonames[0].lat, data.geonames[0].lng];
+		})
+		.then(([lat, lon]) => {
+			// Get weather data and image of destination
+			return Promise.all([
+				getWeatherData(lat, lon),
+				getDestinationImage(destinationData),
+			]);
+		})
+		.then((results) => {
+			console.log(results[0]);
+			const weatherResult = results[0].data[0];
+			// Save weather data to destination data
+			Object.assign(destinationData, {
+				sunrise: weatherResult.sunrise,
+				sunset: weatherResult.sunset,
+				timezone: weatherResult.timezone,
+				pressure: weatherResult.pres,
+				wind_speed: weatherResult.wind_spd,
+				temp: weatherResult.temp,
+				apparent_temp: weatherResult.app_temp,
+				weather: weatherResult.weather.description,
+				icon: weatherResult.weather.icon,
+				wind_dir_code: weatherResult.wind_cdir,
+				humidity: weatherResult.rh,
+			});
+			res.send(destinationData);
+		})
+		.catch((error) => {
+			// Report search parameter not found
+			res.send({ result: "Location not found", found: false });
+			console.log("error", error, "\nLocation not found");
+		});
 
-    // Get geographic data
-    getGeoData(destination)
-    .then(data => {
-        // Save geo data to destination data
-        Object.assign(destinationData, data.geonames[0])
-        // console.log(data.geonames[0])
-        console.log(destinationData)
-        return [data.geonames[0].lat, data.geonames[0].lng]
-    })
-    .then(([lat, lon]) => {
-        // Get weather data and image of destination
-        return Promise.all([getWeatherData(lat, lon), getDestinationImage(destinationData)])
-    })
-    .then(results => {
-        console.log(results)
-        const weatherResult = results[0].data[0]
-        // Save weather data to destination data
-        Object.assign(destinationData, {
-            sunrise: weatherResult.sunrise,
-            sunset: weatherResult.sunset,
-            timezone: weatherResult.timezone,
-            presure: weatherResult.pres,
-            wind_speed: weatherResult.wind_spd,
-            temp: weatherResult.temp,
-            apparent_temp: weatherResult.app_temp,
-            weather: weatherResult.weather.description,
-            icon: weatherResult.weather.icon
-        })
-        res.send(destinationData)   
-    }).catch(error => {
-        // Report search parameter not found
-        res.send({result:"Location not found", found:false})
-        console.log('error', error, "\nLocation not found")
-    })
-    
-    // res.send()
-    
-})
-
+	// res.send()
+});
 
 // Get geographic data
 const getGeoData = async (name) => {
-    const username = process.env.geoNameUsername
-    const geoNameURL = `http://api.geonames.org/search?name=${name}&maxRows=1&type=json&username=${username}`
-    const response = await fetch (encodeURI(geoNameURL))
-    try {
-        return await response.json()
-    } catch (error) {
-        console.log('error', error)
-    }
-}
+	const username = process.env.geoNameUsername;
+	const geoNameURL = `http://api.geonames.org/search?name=${name}&maxRows=1&type=json&username=${username}`;
+	const response = await fetch(encodeURI(geoNameURL));
+	try {
+		return await response.json();
+	} catch (error) {
+		console.log("error", error);
+	}
+};
 
 // Get weather data
-const getWeatherData = async (lat,lon) => {
-    const apiKey = process.env.weatherBitApiKey
-    const url = `https://api.weatherbit.io/v2.0/current?key=${apiKey}&lat=${lat}&lon=${lon}`
-    // console.log(lat, lon)
-    const response = await fetch (encodeURI(url))
-    try {
-        return await response.json()
-    } catch (error) {
-        console.log('error', error)
-    }
-}
+const getWeatherData = async (lat, lon) => {
+	const apiKey = process.env.weatherBitApiKey;
+	const url = `https://api.weatherbit.io/v2.0/current?key=${apiKey}&lat=${lat}&lon=${lon}`;
+	// console.log(lat, lon)
+	const response = await fetch(encodeURI(url));
+	try {
+		return await response.json();
+	} catch (error) {
+		console.log("error", error);
+	}
+};
 
 // Get destination image
 const getDestinationImage = async (destination) => {
-    const apiKey = process.env.pixabayApiKey
-    const destinationName = destination.name
-    const url = `https://pixabay.com/api/?key=${apiKey}&q=${destinationName}&category=places&image_type=photo&pretty=true`
-    console.log(destination.name, destination.countryName)
-    const response = await fetch (encodeURI(url))
-    try {
-        const result = await response.json()
-        destination.image = result.hits[0].webformatURL
-    } catch (error) {
-        console.log('error', error)
-
-    }
-}
+	const apiKey = process.env.pixabayApiKey;
+	const destinationName = destination.name;
+	const url = `https://pixabay.com/api/?key=${apiKey}&q=${destinationName}&category=places&image_type=photo&pretty=true`;
+	console.log(destination.name, destination.countryName);
+	const response = await fetch(encodeURI(url));
+	try {
+		const result = await response.json();
+		destination.image = result.hits[0].webformatURL;
+	} catch (error) {
+		console.log("error", error);
+	}
+};
